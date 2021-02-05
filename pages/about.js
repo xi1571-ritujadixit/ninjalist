f8667083942585b009bb8e2e4ebf6081023ac916
Rituja Dixit <rituja.dixit@xebia.com>
2021-02-05 18:05:58 +0530

    Linking between pages

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, explicabo rem? Possimus perferendis voluptatibus maiores accusamus, nostrum aut nisi necessitatibus magni saepe quisquam aspernatur labore sequi tempore culpa expedita provident.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, explicabo rem? Possimus perferendis voluptatibus maiores accusamus, nostrum aut nisi necessitatibus magni saepe quisquam aspernatur labore sequi tempore culpa expedita provident.</p>
+        </div>
+    );
+}
+ 
+export default About;
