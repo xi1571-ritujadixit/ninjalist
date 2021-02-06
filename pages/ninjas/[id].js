03bf72adfe88b0e5a949632b15011aa26826e7e8
Rituja Dixit <rituja.dixit@xebia.com>
2021-02-06 15:17:34 +0530

    dynamic routes

---
+++ pages/ninjas/[id].js
@@ -0,0 +1,9 @@
+const Deatils = () => {
+    return (
+        <div>
+            <h1>Details Page</h1>
+        </div>
+    );
+}
+ 
+export default Deatils;
--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,28 @@
-const Ninjas = () => {
+import Link from 'next/link';
+import styles from '../../styles/Ninjas.module.css'
+
+export const getStaticProps = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: { ninjas: data }
+    }
+
+}
+
+const Ninjas = ({ ninjas }) => {
     return (
         <div>
             <h1>All Ninjas</h1>
+            {ninjas.map(ninja => (
+                <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
+                    <a className={styles.single}>
+                        <h3>{ ninja.name }</h3>
+                    </a>
+                </Link>
+            ))}
         </div>
     );
 }
