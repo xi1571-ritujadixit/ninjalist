4042f0ed9b6c1ac37a282f029ca9dbdbdcc04a40
Rituja Dixit <rituja.dixit@xebia.com>
2021-02-06 13:00:04 +0530

    images and metadata

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from 'next/head'
+
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, explicabo rem? Possimus perferendis voluptatibus maiores accusamus, nostrum aut nisi necessitatibus magni saepe quisquam aspernatur labore sequi tempore culpa expedita provident.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, explicabo rem? Possimus perferendis voluptatibus maiores accusamus, nostrum aut nisi necessitatibus magni saepe quisquam aspernatur labore sequi tempore culpa expedita provident.</p>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas"/>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, explicabo rem? Possimus perferendis voluptatibus maiores accusamus, nostrum aut nisi necessitatibus magni saepe quisquam aspernatur labore sequi tempore culpa expedita provident.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, explicabo rem? Possimus perferendis voluptatibus maiores accusamus, nostrum aut nisi necessitatibus magni saepe quisquam aspernatur labore sequi tempore culpa expedita provident.</p>
+            </div>
+        </>
     );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,19 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, explicabo rem? Possimus perferendis voluptatibus maiores accusamus, nostrum aut nisi necessitatibus magni saepe quisquam aspernatur labore sequi tempore culpa expedita provident.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, explicabo rem? Possimus perferendis voluptatibus maiores accusamus, nostrum aut nisi necessitatibus magni saepe quisquam aspernatur labore sequi tempore culpa expedita provident.</p>
-      <Link href = "/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, explicabo rem? Possimus perferendis voluptatibus maiores accusamus, nostrum aut nisi necessitatibus magni saepe quisquam aspernatur labore sequi tempore culpa expedita provident.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, explicabo rem? Possimus perferendis voluptatibus maiores accusamus, nostrum aut nisi necessitatibus magni saepe quisquam aspernatur labore sequi tempore culpa expedita provident.</p>
+        <Link href = "/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   )
 }
